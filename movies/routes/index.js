624e6de28ae1b3b3047c8e79c9ca39225ebfc6ba
Radhika Mattoo <rm3485@nyu.edu>
2016-04-26 21:23:57 -0400

    starting

--- movies/routes/index.js
+++ movies/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Movie = mongoose.model('Movie');
+var req = new XMLHttpRequest();
 
 router.get('/', function(req, res) {
   res.redirect('/movies');
@@ -10,29 +11,29 @@ router.get('/', function(req, res) {
 router.get('/movies', function(req, res) {
   var movieFilter = {},
     searchExists = false;
-  
+
   if(req.query.director) {
-    movieFilter.director = req.query.director; 
+    movieFilter.director = req.query.director;
     searchExists = true;
   }
- 
+
   Movie.find(movieFilter, function(err, movies, count) {
     res.render('movies', {'movies': movies, searchExists: searchExists, director: req.query.director });
   });
 });
 
-router.post('/movies/create', function(req, res) {
-  (new Movie({
-      title: req.body.movieTitle,
-      director: req.body.movieDirector,
-      year: req.body.movieYear
-  })).save(function(err, movie, count) {
-    res.redirect('/movies');
-  });
-});
+// router.post('/movies/create', function(req, res) {
+//   (new Movie({
+//       title: req.body.movieTitle,
+//       director: req.body.movieDirector,
+//       year: req.body.movieYear
+//   })).save(function(err, movie, count) {
+//     res.redirect('/movies');
+//   });
+// });
 
-router.get('/movies/create', function(req, res) {
-  res.render('movies-create', {}); 
-});
+// router.get('/movies/create', function(req, res) {
+//   res.render('movies-create', {});
+// });
 
 module.exports = router;
